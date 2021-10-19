f6c442aba9f2637b7531f983160253f66abce831
P22inolan <p22inolan@fairfieldprep.org>
2021-10-19 10:44:01 -0400

    Submit 10/19

---
+++ quiz-1-part-2.py
@@ -0,0 +1,48 @@
+# Author: IBN (AMDG) 10/19/2021
+
+q = int(input("What was the day of the month? "))
+m = int(input("What was the month? "))
+y = int(input("What was the year? "))
+if m == 1:
+    m = 13
+if m == 2:
+    m = 14
+else:
+    m = m
+if m == 13:
+    y -= 1
+if m == 14:
+    y -= 1
+j = int(y / 100)
+k = int(y % 100)
+
+h = int(((q + ((26 * (m+1)) // 10) + k + (k // 4) + (j // 4) + (5 * j)) % 7))
+
+if h == 0:
+    h = "Saturday"
+elif h == 1:
+    h = "Sunday"
+elif h == 2:
+    h = "Monday"
+elif h == 3:
+    h = "Tuesday"
+elif h == 4:
+    h = "Wednesday"
+elif h == 5:
+    h = "Thursday"
+elif h == 6:
+    h = "Friday"
+
+if m == 13:
+    m = 1
+if m == 14:
+    m = 2
+else:
+    m = m
+if m == 1:
+    y += 1
+if m == 2:
+    y += 1
+print(str(m) + "/" + str(q) + "/" + str(y) + " was a: " + str(h))
+
+# This is needlessly complicated but it works
